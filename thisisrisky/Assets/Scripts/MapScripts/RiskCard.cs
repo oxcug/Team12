03ddcf01919e9b30e1b13fefe5b3a19a07f81f35
adagiox <adagiox@users.noreply.github.com>
2019-04-09 21:27:34 -0700

    Randomly Generated Risk Cards!

--- thisisrisky/Assets/Scripts/MapScripts/RiskCard.cs
+++ thisisrisky/Assets/Scripts/MapScripts/RiskCard.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class RiskCard : MonoBehaviour
 {
+    public GameObject CardPrefab;
+    public List<GameObject> RiskCards = new List<GameObject>();
+    public GameObject TempCard;
+    public Sprite[] TerritorySprites;
+    public Sprite[] UnitTypeSprites;
 
-    public GameObject card;
-
-    // Start is called before the first frame update
     void Start()
     {
-            
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void NewCard()
+    {
+        if (TempCard)
+        {
+            Destroy(TempCard);
+        }
+        GameObject Card = Instantiate(CardPrefab, new Vector3(2.56f, -2.93f, -0.81f), Quaternion.identity) as GameObject;
+        Card.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().sprite = UnitTypeSprites[Random.Range(0, UnitTypeSprites.Length)];
+        Card.transform.GetChild(1).gameObject.GetComponent<SpriteRenderer>().sprite = TerritorySprites[Random.Range(0, TerritorySprites.Length)];
+        TempCard = Card;
     }
 }
