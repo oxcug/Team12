09a928267e62df3e077421af3e52e794ccf89e9b
adagiox <adagiox@users.noreply.github.com>
2019-04-09 20:00:06 -0700

    Risk Cards, Battle Camera Toggle
    Added button to test generating new Risk Card objects and a button to switch camera views to a 'battle' view/camera. Made a prefab for a Risk Card which when instantiated will choose a random territory and unit type via script.

---
+++ thisisrisky/Assets/Scripts/MapScripts/ToggleBattleCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleBattleCamera : MonoBehaviour
+{
+    public Camera MainCamera;
+    public Camera BattleCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainCamera = Camera.main;
+        MainCamera.enabled = true;
+        BattleCamera.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ToggleCamera()
+    {
+        if (MainCamera.enabled)
+        {
+            BattleCamera.enabled = true;
+            MainCamera.enabled = false;
+        }
+        else if (!MainCamera.enabled)
+        {
+            BattleCamera.enabled = false;
+            MainCamera.enabled = true;
+        }
+    }
+}
