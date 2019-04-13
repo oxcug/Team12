6b6a05ed26b728933e9222c4955aba2d2f2529a6
adagiox <adagiox@users.noreply.github.com>
2019-04-12 23:09:22 -0700

    Added Start Menu and Simple Game UI
    Added a new scene for the start menu with options for 1, 2, 3, 4, and 5 players. The start button will change scenes to the game scene with the map. Number of players is not being passed through to the game, but should be easy to add.

--- thisisrisky/Assets/Scripts/MapScripts/RiskCard.cs
+++ thisisrisky/Assets/Scripts/MapScripts/RiskCard.cs
@@ -7,8 +7,7 @@ public class RiskCard : MonoBehaviour
     public GameObject CardPrefab;
     public List<GameObject> RiskCards = new List<GameObject>();
     public GameObject TempCard;
-    public Sprite[] TerritorySprites;
-    public Sprite[] UnitTypeSprites;
+    public Sprite[] CardSprites;
 
     void Start()
     {
@@ -23,13 +22,16 @@ public class RiskCard : MonoBehaviour
 
     public void NewCard()
     {
+        // Discard the previous card created (just for testing)
         if (TempCard)
         {
             Destroy(TempCard);
         }
-        GameObject Card = Instantiate(CardPrefab, new Vector3(2.56f, -2.93f, -0.81f), Quaternion.identity) as GameObject;
-        Card.transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().sprite = UnitTypeSprites[Random.Range(0, UnitTypeSprites.Length)];
-        Card.transform.GetChild(1).gameObject.GetComponent<SpriteRenderer>().sprite = TerritorySprites[Random.Range(0, TerritorySprites.Length)];
+        Debug.Log(Camera.main);
+        Vector2 cardVec = Camera.main.ViewportToWorldPoint(new Vector2(Random.value, Random.value));
+        GameObject Card = Instantiate(CardPrefab, cardVec, Quaternion.identity) as GameObject;
+        Card.GetComponent<SpriteRenderer>().sprite = CardSprites[Random.Range(0,CardSprites.Length)];
         TempCard = Card;
+        Debug.Log("NewCard: " + TempCard.transform.position + "\n" + TempCard.name);
     }
 }
